2c92be4fa37ab79f69e813e421e47cdc62449f1d
s82626 <s82626@informatik.htw-dresden.de>
2022-07-04 15:34:27 +0200

    text

--- speech-recognition.js
+++ speech-recognition.js
@@ -20,61 +20,61 @@ document.getElementById('start-voice-recognition').addEventListener('click', fun
     recognition.start();
     console.log('Ready to receive a command.');
 
-recognition.onresult = function(event) {
-    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-    // It has a getter so it can be accessed like an array
-    // The first [0] returns the SpeechRecognitionResult at position 0.
-    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-    // These also have getters so they can be accessed like arrays.
-    // The second [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object
-    var voiceCommand = event.results[i][0].transcript;
-    i++;
-    //diagnostic.textContent = 'Result received: ' + voiceCommand + '.';
-    console.log("voice command " + voiceCommand);
-    if(voiceCommand.includes("Alex") || voiceCommand.includes("alex")){
-     if(voiceCommand.includes("start") || voiceCommand.includes("Start")) {
-        factory.setAttribute('animation-mixer', {timeScale: 1});
-        console.log("factory starts...");
-     }
-        else if(voiceCommand.includes("stop") || voiceCommand.includes("Stop")) {
-            factory.setAttribute('animation-mixer', {timeScale: 0});
-            console.log("factory stops...");
+    recognition.onresult = function(event) {
+        // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+        // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+        // It has a getter so it can be accessed like an array
+        // The first [0] returns the SpeechRecognitionResult at position 0.
+        // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+        // These also have getters so they can be accessed like arrays.
+        // The second [0] returns the SpeechRecognitionAlternative at position 0.
+        // We then return the transcript property of the SpeechRecognitionAlternative object
+        var voiceCommand = event.results[i][0].transcript;
+        i++;
+        //diagnostic.textContent = 'Result received: ' + voiceCommand + '.';
+        console.log("voice command " + voiceCommand);
+        if(voiceCommand.includes("Alex") || voiceCommand.includes("alex")){
+        if(voiceCommand.includes("start") || voiceCommand.includes("Start")) {
+            factory.setAttribute('animation-mixer', {timeScale: 1});
+            console.log("factory starts...");
+        }
+            else if(voiceCommand.includes("stop") || voiceCommand.includes("Stop")) {
+                factory.setAttribute('animation-mixer', {timeScale: 0});
+                console.log("factory stops...");
+            }
+
+            else if(voiceCommand.includes("schneller") || voiceCommand.includes("Schneller")) {
+                factory.setAttribute('animation-mixer', {timeScale: timeScale + 0.5});
+                console.log("factory schneller...");
+            }
+
+            else if(voiceCommand.includes("langsamer") || voiceCommand.includes("Langsamer")) {
+                factory.setAttribute('animation-mixer', {timeScale: timeScale - 0.5});
+                console.log("factory langsamer...");
+            }
+
+            else if(voiceCommand.includes("größer") || voiceCommand.includes("Größer")) {
+            //increase size glb object
+                factory.setAttribute('scale', {x: 1.5, y: 1.5, z: 1.5});
+                console.log("factory größer...");
+            }
+
+            else if(voiceCommand.includes("kleiner") || voiceCommand.includes("Kleiner")) {
+            //decrease size glb object
+                factory.setAttribute('scale', {x: 0.5, y: 0.5, z: 0.5});
+                console.log("factory kleiner...");
+            }
+
+            else {
+                console.log("Alex says: unsupported voice command");
+        }
+        //reset transcript
+        voiceCommand = "";
         }
 
-        else if(voiceCommand.includes("schneller") || voiceCommand.includes("Schneller")) {
-            factory.setAttribute('animation-mixer', {timeScale: timeScale + 0.5});
-            console.log("factory schneller...");
+            //factory.setAttribute('voiceCommand', voiceCommand);
+            console.log('Confidence: ' + event.results[0][0].confidence);
         }
-
-        else if(voiceCommand.includes("langsamer") || voiceCommand.includes("Langsamer")) {
-            factory.setAttribute('animation-mixer', {timeScale: timeScale - 0.5});
-            console.log("factory langsamer...");
-        }
-
-        else if(voiceCommand.includes("größer") || voiceCommand.includes("Größer")) {
-        //increase size glb object
-            factory.setAttribute('scale', {x: 1.5, y: 1.5, z: 1.5});
-            console.log("factory größer...");
-        }
-
-        else if(voiceCommand.includes("kleiner") || voiceCommand.includes("Kleiner")) {
-        //decrease size glb object
-            factory.setAttribute('scale', {x: 0.5, y: 0.5, z: 0.5});
-            console.log("factory kleiner...");
-        }
-
-        else {
-            console.log("Alex says: unsupported voice command");
-    }
-    //reset transcript
-    voiceCommand = "";
-    }
-
-        //factory.setAttribute('voiceCommand', voiceCommand);
-        console.log('Confidence: ' + event.results[0][0].confidence);
-    }
 });
 
 recognition.stop();
