1641bbeb8b4b3cfdd4cb9a455802bd11a76e132f
Leander.Strzata <s80585@informatik.htw-dresden.de>
2022-07-07 10:41:09 +0200

    Slight Changes, Production count

--- animation-mixer.js
+++ animation-mixer.js
@@ -44,26 +44,39 @@ module.exports = AFRAME.registerComponent("animation-mixer", {
 
   load: function (model) {
     const el = this.el;
+    var count = 0;
+    var text = document.querySelector('a-text');
     this.model = model;
     this.mixer = new THREE.AnimationMixer(model);
     this.mixer.addEventListener("loop", (e) => {
       el.emit("animation-loop", { action: e.action, loopDelta: e.loopDelta });
+      if(e.action._clip.name == "SchraubeEnd") {
+        count++;
+        text.setAttribute("value", "Produziert: " + count)
+        console.log("COUNT "+count);
+        
+      }
+      console.log(e.action._clip.name);
     });
     this.mixer.addEventListener("finished", (e) => {
       el.emit("animation-finished", {
         action: e.action,
         direction: e.direction,
       });
+      console.log("finished");
     });
     if (this.data.clip) this.update({});
   },
 
   remove: function () {
+    console.log("remove");
     if (this.mixer) this.mixer.stopAllAction();
   },
 
   update: function (prevData) {
+    console.log("update");
     if (!prevData) return;
+    console.log("update 2nd");
 
     const data = this.data;
     const changes = AFRAME.utils.diff(data, prevData);
@@ -74,22 +87,28 @@ module.exports = AFRAME.registerComponent("animation-mixer", {
       if (data.clip) this.playAction();
       return;
     }
-
+    console.log("update mid");
     // Otherwise, modify running actions.
     this.activeActions.forEach((action) => {
+      console.log("modify running actions");
       if ("duration" in changes && data.duration) {
         action.setDuration(data.duration);
+        console.log("durations end");
       }
       if ("clampWhenFinished" in changes) {
         action.clampWhenFinished = data.clampWhenFinished;
+        console.log("clamp end");
       }
       if ("loop" in changes || "repetitions" in changes) {
         action.setLoop(LoopMode[data.loop], data.repetitions);
+        console.log("loop & repetitions end");
       }
       if ("timeScale" in changes) {
         action.setEffectiveTimeScale(data.timeScale);
+        console.log("time scale end");
       }
     });
+    console.log("update end");
   },
 
   stopAction: function () {
@@ -103,6 +122,7 @@ module.exports = AFRAME.registerComponent("animation-mixer", {
   },
 
   playAction: function () {
+    console.log("playAction");
     if (!this.mixer) return;
 
     const model = this.model,
@@ -116,7 +136,7 @@ module.exports = AFRAME.registerComponent("animation-mixer", {
     for (let clip, i = 0; (clip = clips[i]); i++) {
       if (clip.name.match(re)) {
         const action = this.mixer.clipAction(clip, model);
-
+        
         action.enabled = true;
         action.clampWhenFinished = data.clampWhenFinished;
         if (data.duration) action.setDuration(data.duration);
@@ -129,10 +149,12 @@ module.exports = AFRAME.registerComponent("animation-mixer", {
         this.mixer.setTime(data.startFrame / 1000);
       }
     }
+    console.log("play end");
   },
 
   tick: function (t, dt) {
     if (this.mixer && !isNaN(dt)) this.mixer.update(dt / 1000);
+    // console.log("tick");
   },
 });
 
